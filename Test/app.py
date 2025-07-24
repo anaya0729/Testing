6a0a0e671e74d6ddeda5e69bc5d7a7c47bc32687
anaya0729 <anaya@mu-sigma.com>
2025-07-24 09:14:25 +0000

    Adding flask changes

--- Test/app.py
+++ Test/app.py
@@ -1,1 +1,10 @@
-print("Hello from  Python inside Docker!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello This is my app!"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
